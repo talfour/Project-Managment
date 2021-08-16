a65a6621c62aa5c62ce6b7f1c79ef777242648f6
talfour <jak.malinowski94@gmail.com>
2021-08-16 16:10:25 +0100

    added message function

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,60 +1,88 @@
-let card = NaN
+let card = NaN;
 const csrftoken = getCookie("csrftoken");
-let target_id = 0
+let target_id = 0;
 
-function allowDrop(ev){
-    ev.preventDefault();
+function insertAfter(referenceNode, newNode) {
+	referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-function drag(ev){
-    ev.dataTransfer.setData("text", ev.target.dataset.id)
-    card = ev.target
-    return target_id = ev.target.dataset.id
+function addMessage(messageText, messageType) {
+	const nav = document.getElementsByClassName("navbar");
+	let messExists = document.getElementById("message");
+    console.log(messExists)
+    if(messExists){
+        messExists.remove()
+    }
+	let message = document.createElement("div");
+	message.setAttribute("id", "message");
+	message.innerHTML = `<div class="alert alert-${messageType} alert-dismissible fade show text-center" role="alert">
+    ${messageText}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`;
+	insertAfter(nav[0], message);
 }
 
-function drop(ev){
-    ev.preventDefault();
-    fetch(`/project/task-status-change/`, {
-        credentials: "include",
-        method: "POST",
-        mode: "same-origin",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken,
-        },
-        body: JSON.stringify({
-            task_id: target_id,
-            new_status: ev.target.dataset.status,
-        }),
-    })
-    .then((response) => response.json())
-    .then((response) => {
-        if(response.message == "Task status changed"){
-            ev.target.appendChild(card);
-        }
-    })
-    
+function allowDrop(ev) {
+	ev.preventDefault();
 }
 
-function assign_me(ev){
-    ev.preventDefault();
-    task_id = ev.explicitOriginalTarget.parentElement.parentElement.dataset.id
-    fetch(`/project/task-assign-user/`, {
-        credentials: "include",
-        method: "POST",
-        mode: "same-origin",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "X-CSRFToken": csrftoken,
-        },
-        body: JSON.stringify({
-            task_id: task_id,
-        }),
-    })
-    .then((response) => response.json())
-    .then((response) => {
-        ev.target.remove()
-    })
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.dataset.id);
+	card = ev.target;
+	return (target_id = ev.target.dataset.id);
+}
+
+function drop(ev) {
+	ev.preventDefault();
+	fetch(`/project/task-status-change/`, {
+		credentials: "include",
+		method: "POST",
+		mode: "same-origin",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			"X-CSRFToken": csrftoken,
+		},
+		body: JSON.stringify({
+			task_id: target_id,
+			new_status: ev.target.dataset.status,
+		}),
+	})
+		.then((response) => response.json())
+		.then((response) => {
+			if (response.message == "Task status changed") {
+				ev.target.appendChild(card);
+				addMessage(response.message, "success");
+			} else {
+				addMessage(response.message, "danger");
+			}
+			console.log(response);
+		});
+}
+
+function assign_me(ev) {
+	ev.preventDefault();
+	console.log(ev);
+	task_id = ev.explicitOriginalTarget.parentElement.parentElement.dataset.id;
+	if (task_id == null) {
+		task_id = ev.explicitOriginalTarget.dataset.id;
+	}
+
+	fetch(`/project/task-assign-user/`, {
+		credentials: "include",
+		method: "POST",
+		mode: "same-origin",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			"X-CSRFToken": csrftoken,
+		},
+		body: JSON.stringify({
+			task_id: task_id,
+		}),
+	})
+		.then((response) => response.json())
+		.then((response) => {
+			ev.target.remove();
+		});
 }
